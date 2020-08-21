2fab570d0427c11b750c22b2880b729d79c6c302
Osiel Nicolás <chilparosales@gmail.com>
2020-08-21 18:56:52 -0500

    Elimando archivos innecesarios

--- HTML/estructura/JS/scripts.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('h1').addEventListener('click', () => {  /* Selecciona dentro del doc, h1 y agregale el evento clik */
-    document.body.style.background = "white";  /* Dentro del cuerpo y el estilo cambiar el color de fondo */
-})
